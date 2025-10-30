f45e8a8da2ce0235816134b1db2183783ed5ca24
Michael Borges <michaelborges21@gmail.com>
2025-10-30 11:35:18 -0300

    fazendo limpeza nas bibliotecas, limpando o projeto todo

--- src/keys.py
+++ src/keys.py
@@ -1,25 +1,20 @@
 import os
 from yaml import safe_load
 
-def tokens():
+def tokens()->str:
     PATH = '../key.yaml'  # arquivo em formato string e não dicionario 
 
     with open(PATH) as token_f:
         # token_data é o dicionário carregado do YAML
         token_data = safe_load(token_f) 
     
-    # [CORREÇÃO 1] Extrair a string do dicionário
-    # Vamos garantir que é uma string
-    token_string = str(token_data)
-
-    # [CORREÇÃO 1 - Continuação] Atribuir a STRING à variável de ambiente
-    os.environ['HUGGING_API_TOKEN'] = token_string
-
+    # Extrair a string do dicionário
+    token_string = token_data['TOKEN']
+   
+    # Atribuir a STRING à variável de ambiente
+    os.environ['TOKEN'] = token_string
+    
     print(f">>>TOKEN ACESSADO COM SUCESSO<<")
     
-    # Vamos verificar se o OS "aprendeu" a variável (apenas dentro deste script)
-    print("Verificando a variável no os.environ:")
-    
-    # print(os.getenv("HUGGING_API_TOKEN"))
-    
-
+    T = token_string[0:8]
+    return T
